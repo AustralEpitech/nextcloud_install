482a20d520bb470be9d791b10581da5581261ef5
angeD <ange.duhayon@epitech.eu>
2022-06-19 15:10:06 +0200

    feat: full docker config

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
 # nextcloud_install for debian
 
-> **Warning**: set SQL_LOGIN and SQL_PASSWD before launching the script or
-> unsafe default values will be set:
+## HOW-TO
 ```console
-export SQL_LOGIN="login"
-export SQL_PASSWD="passwd"
-export SERVER_NAME="your.server.com"
+git clone https://github.com/AustralEpitech/nextcloud_install.git
 ```
-
-then, run installation script:
-```console
-curl https://raw.githubusercontent.com/AustralEpitech/nextcloud_install/main/install.sh | sudo bash
-```
-
 
 # TODO:
-- [ ] pretty urls
+- [ ] Remove apache redundancy
+- [ ] nginx ?
--- install.sh
+++ install.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/bash
+set -e
 
-a2enmod proxy proxy_http
+DEPS=(
+    apache2
+    certbot
+    docker-compose
+    docker.io
+    python3-certbot-apache
+)
+
+[ "$EUID" != 0 ] && sudo -- "$0" "$@"
+
+cat ./env
+
+read -rp 'Are these values correct? (Ctrl-C to cancel) '
+
+source ./env
+
+apt update && apt upgrade
+apt install "${DEPS[@]}"
+
+for FILE in docker-compose.yaml nextcloud*.conf; do
+    envsubst < "$FILE" > "$FILE"
+done
+
+cp -f nextcloud.conf /etc/apache2/sites-available
+a2enmod nextcloud.conf
+
+certbot --apache -d "$SERVER_URL"
+
+cp -f nextcloud-le-ssl.conf /etc/apache2/sites-available
+
+a2enmod rewrite proxy proxy_http
+a2ensite nextcloud-le-ssl.conf
+docker-compose up -d
+
+echo -e '\e[32mDONE\e[0m'
