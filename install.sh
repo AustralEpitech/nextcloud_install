bd61e7d493c49bd9b9a11577ce69779528c9f0b0
angeD <ange.duhayon@epitech.eu>
2022-06-19 15:46:19 +0200

    fix: better automation and variables

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 set -e
 
+CP="cp -f"
+
 DEPS=(
     apache2
     certbot
@@ -17,22 +19,21 @@ read -rp 'Are these values correct? (Ctrl-C to cancel) '
 
 source ./env
 
-apt update && apt upgrade
-apt install "${DEPS[@]}"
+apt update && apt upgrade -y
+apt install -y "${DEPS[@]}"
 
 for FILE in docker-compose.yaml nextcloud*.conf; do
     envsubst < "$FILE" > "$FILE"
 done
 
-cp -f nextcloud.conf /etc/apache2/sites-available
-a2enmod nextcloud.conf
+$CP nextcloud.conf /etc/apache2/sites-available
 
-certbot --apache -d "$SERVER_URL"
+certbot certonly -n --apache -d "$SERVER_URL" -m "$SERVER_MAIL" --agree-tos --test-cert
 
-cp -f nextcloud-le-ssl.conf /etc/apache2/sites-available
+$CP nextcloud-le-ssl.conf /etc/apache2/sites-available
 
 a2enmod rewrite proxy proxy_http
-a2ensite nextcloud-le-ssl.conf
+a2ensite nextcloud.conf nextcloud-le-ssl.conf
 docker-compose up -d
 
 echo -e '\e[32mDONE\e[0m'
